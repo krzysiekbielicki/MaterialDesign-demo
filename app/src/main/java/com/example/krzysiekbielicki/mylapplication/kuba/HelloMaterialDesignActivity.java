3dd8c446ef804b529249453ce53bf3e2bef1d4fb
Jakub Krolewski <jakub.krolewski@allegrogroup.com>
2014-08-26 14:17:04 +0200

    Adding HelloMaterialDesignActivity

---
+++ app/src/main/java/com/example/krzysiekbielicki/mylapplication/kuba/HelloMaterialDesignActivity.java
@@ -0,0 +1,15 @@
+package com.example.krzysiekbielicki.mylapplication.kuba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.krzysiekbielicki.mylapplication.R;
+
+public class HelloMaterialDesignActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_hello_material);
+    }
+}
