40f25bcb51f309661fb8b3bbddc92f2c90550cb9
Jakub Krolewski <jakub.krolewski@allegrogroup.com>
2014-08-26 14:44:15 +0200

    DemoListActivity

---
+++ app/src/main/java/com/example/krzysiekbielicki/mylapplication/kuba/DemoListActivity.java
@@ -0,0 +1,30 @@
+package com.example.krzysiekbielicki.mylapplication.kuba;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.krzysiekbielicki.mylapplication.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DemoListActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_demo_list);
+
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.demo_hello)
+    protected void onDemoHelloButtonClick() {
+       startActivity(HelloMaterialDesignActivity.class);
+    }
+
+    private void startActivity(Class<? extends Activity> activityClass) {
+        startActivity(new Intent(this, activityClass));
+    }
+}
